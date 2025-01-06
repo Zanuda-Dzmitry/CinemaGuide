aec77cb6cfaf3102169231c1d6144d0d1a898abf
Dzmitry_Shaviaka <dcadaver@yandex.ru>
2025-01-06 18:23:29 +0300

    test favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
 					href: 'https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap',
 					rel: 'stylesheet',
 				},
-				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+				{ rel: 'icon', type: 'image/x-icon', href: '/CinemaGuide/favicon.ico' },
 			],
 		},
 		baseURL: '/CinemaGuide/',
