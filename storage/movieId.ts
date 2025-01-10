800ed4a64a11b17333838c452d0db8c8717eae88
Dzmitry_Shaviaka <dcadaver@yandex.ru>
2025-01-10 18:22:48 +0300

    добавил вывод ошибок на экран, доработал типизацию

--- storage/movieId.ts
+++ storage/movieId.ts
@@ -1,46 +1,16 @@
-import type { MovieType } from '../services/types/types'
+import type { Movie } from '../services/types/types'
 import { URL_MOVIE } from '~/constants'
 import axios from 'axios'
 
 export const useMovieId = defineStore('movie', {
 	state: () => ({
-		movieId: 0,
-		movieTitle: '',
-		moviePlot: '',
-		movieRating: 0,
-		movieYear: 0,
-		movieGenre: [],
-		movieRuntime: 0,
-		moviePoster: '',
-		movieBackdrop: '',
-		movieBudget: '',
-		movieLanguage: '',
-		movieRevenue: '',
-		movieDirector: '',
-		movieProduction: '',
-		movieAwardsSummary: '',
-		movieTrailerYouTubeId: '',
+		movie: {} as Movie,
 	}),
 
 	actions: {
 		async fetchMovieId(genreId: string) {
-			const response = await axios.get<MovieType>(`${URL_MOVIE}/${genreId}`)
-			this.movieId = response.data.id
-			this.movieTitle = response.data.title
-			this.moviePlot = response.data.plot
-			this.movieRating = response.data.tmdbRating
-			this.movieYear = response.data.releaseYear
-			this.movieGenre = response.data.genres
-			this.movieRuntime = response.data.runtime
-			this.moviePoster = response.data.posterUrl
-			this.movieBackdrop = response.data.backdropUrl
-			this.movieBudget = response.data.budget
-			this.movieLanguage = response.data.language
-			this.movieRevenue = response.data.revenue
-			this.movieDirector = response.data.director
-			this.movieProduction = response.data.production
-			this.movieAwardsSummary = response.data.awardsSummary
-			this.movieTrailerYouTubeId = response.data.trailerYouTubeId
+			const response = await axios.get(`${URL_MOVIE}/${genreId}`)
+			this.movie = response.data
 		},
 	},
 })
